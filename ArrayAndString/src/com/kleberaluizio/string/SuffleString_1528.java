f598faa6e40d01c89e47d7d38de76b4389594d2d
kleberaluizio <kleberaluizio@gmail.com>
2023-07-31 15:51:14 -0300

    Solving new challenges

---
+++ ArrayAndString/src/com/kleberaluizio/string/SuffleString_1528.java
@@ -0,0 +1,19 @@
+package com.kleberaluizio.string;
+
+public class SuffleString_1528 {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int [] indices = {4,5,6,7,0,2,1,3};
+        leetc
+
+        System.out.println(restoreString(s,indices));
+    }
+    public static String restoreString(String s, int[] indices) {
+        String output = "";
+
+        for(int i = 0; i < indices.length; i++){
+            output = output + s.charAt(indices[i]);
+        }
+        return output;
+    }
+}
