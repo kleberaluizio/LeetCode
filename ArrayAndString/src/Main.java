a0d7186ac4da7523abf82c3f6fc78fb08b06904e
kleberaluizio <kleberaluizio@gmail.com>
2023-08-06 19:02:34 -0300

    suffle String 1528 solved

--- ArrayAndString/src/Main.java
+++ ArrayAndString/src/Main.java
@@ -1,62 +1,35 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-//        int[] arr = {1,0,2,3,0,4,5,0};
-//        int[] arr = {1,2,3};
-          int[] arr = {2,5,1,3,4,7};
 
+        String a = "11";
 
-        suffle(arr, 3);
+        String b = "1";
 
+        int valueA = 0;
+        int valueB = 0;
+        int sum = 0;
 
-    }
-
-    public static void duplicateZeros(int[] arr) {
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                int j = arr.length - 1;
-                while (i < j) {
-                    arr[j] = arr[j - 1];
-                    j--;
-                }
-                i++;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) == '1') {
+                valueA += Math.pow(2, i);
             }
         }
-    }
-
-    public static int[] suffle(int [] nums, int n ){
-        int[] x = new int[n];
-        int[] y = new int[n];
-
-        for(int i = 0; i < n; i++){
-            x[i] = nums[i];
-            y[i] = nums[i+n];
-        }
-        int a = 0;
-        int b = 0;
-
-        for(int i = 0; i < 2*n; i++){
-            if(i%2 == 0){
-                nums[i] = x[a];
-                a++;
-            } else {
-                nums[i] = y[b];
-                b++;
+        for(int i = 0; i < b.length(); i++){
+            if(a.charAt(i) == '1') {
+                valueB += Math.pow(2, i);
             }
         }
 
-        for (int i : nums) {
-            System.out.println(i);
-        }
-        System.out.println("---");
-        for (int i : x) {
-            System.out.println(i);
-        }
-        System.out.println("---");
+        sum = valueA + valueB;
 
-        for (int i : y) {
-            System.out.println(i);
-        }
-        return nums;
+        System.out.println(2%2);
+
+        System.out.println(valueA + " + " + valueB + " igual " + sum );
+
+
+
     }
+
 }
--- ArrayAndString/src/com/kleberaluizio/string/SuffleString_1528.java
+++ ArrayAndString/src/com/kleberaluizio/string/SuffleString_1528.java
@@ -4,7 +4,7 @@ public class SuffleString_1528 {
     public static void main(String[] args) {
         String s = "codeleet";
         int [] indices = {4,5,6,7,0,2,1,3};
-        leetc
+
 
         System.out.println(restoreString(s,indices));
     }
